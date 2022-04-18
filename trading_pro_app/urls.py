3afbb551543f8ac80d5b7165eb64c358bb3ebb37
Robert <rfgraves86@gmail.com>
2022-04-18 17:24:50 -0600

    formulated calculator > fix integer inputs

--- trading_pro_app/urls.py
+++ trading_pro_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index),
     path('philosophy', views.philosophy),
     path('calculate', views.calculate),
+    path('trade_setup', views.trade_setup),
     path('risk_management', views.risk),
     path('master', views.master),
 ]
--- trading_pro_app/views.py
+++ trading_pro_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from multiprocessing import context
+from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, "index.html")
@@ -12,12 +13,33 @@ def calculate(request):
 def risk(request):
     return render(request, "risk_management.html")   
 
-
-#This will need to be adjusted so the #'s from input boxes = the parameters. This is also going to redirect back to same page with results. Will need a "Clear/Reset" button feature.
 def master(request):
-    # context = {
-    #     'entry': entry,
-    #     'stop_loss': stop_loss,
-    #     'take_profit': take_profit
-    # }
-    return render(request,"trade_stats.html")
+    request.session['account_size'] = request.POST['account_size']
+    request.session['account_risk_percent'] = request.POST['account_risk_percent']
+    request.session['entry'] = request.POST['entry']
+    request.session['stop_loss'] = request.POST['stop_loss']
+    request.session['take_profit'] = request.POST['take_profit'] 
+
+    request.session['profit_per_share'] = int(request.session['take_profit']) - int(request.session['entry'])
+
+    request.session['stop_loss_per_share'] = int(request.session['entry']) - int(request.session['stop_loss'])
+
+    request.session['risk_to_reward_ratio'] = int(request.session['profit_per_share']) / int(request.session['stop_loss'])
+
+    request.session['return_on_trade'] = (int(request.session['take_profit']) - int(request.session['entry']) / int(request.session['entry']))
+
+    request.session['total_position_size'] = int(request.session['account_risk_percent']) / int(request.session['stop_loss_per_share'])
+
+
+    # profit_per_share = request.session['take_profit'] - (request.session['entry']
+    # stop_loss_per_share = request.session['entry'] - request.session['stop_loss']
+    # risk_to_reward_ratio =  profit_per_share / stop_loss_per_share
+    # return_on_trade = request.session['take_profit'] - request.session['entry'] / request.session['entry']
+    # total_position_size = request.session['account_risk_percent'] / 'stop_loss_per_share'
+   
+    
+    return redirect ('/trade_setup')
+
+
+def trade_setup(request):
+    return render(request, 'trade_stats.html')    
