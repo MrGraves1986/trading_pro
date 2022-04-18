994ddeef2638aec9ee90b90b03923c49b3900a13
Robert <rfgraves86@gmail.com>
2022-04-17 19:47:20 -0600

    routed stat page, setup trade reminder div

--- trading_pro_app/views.py
+++ trading_pro_app/views.py
@@ -14,4 +14,10 @@ def risk(request):
 
 
 #This will need to be adjusted so the #'s from input boxes = the parameters. This is also going to redirect back to same page with results. Will need a "Clear/Reset" button feature.
-def master(request, ):
+def master(request):
+    # context = {
+    #     'entry': entry,
+    #     'stop_loss': stop_loss,
+    #     'take_profit': take_profit
+    # }
+    return render(request,"trade_stats.html")
