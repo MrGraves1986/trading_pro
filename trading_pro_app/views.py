13308a2cf0400e516ab114dabf49978505a24f0a
Robert <rfgraves86@gmail.com>
2022-04-20 17:58:04 -0600

    finished calculator > began risk management

--- trading_pro_app/views.py
+++ trading_pro_app/views.py
@@ -1,5 +1,6 @@
 from multiprocessing import context
 from django.shortcuts import render, redirect
+# import locale ***Need to explore this more and settting it up
 
 def index(request):
     return render(request, "index.html")
@@ -20,19 +21,23 @@ def master(request):
     request.session['stop_loss'] = float(request.POST['stop_loss'])
     request.session['take_profit'] = float(request.POST['take_profit']) 
 
-    request.session['dollar_risk_on_trade'] = float(request.session['account_size']) * float(request.session['account_risk_percent'])
+    request.session['dollar_risk_on_trade'] = format((request.session['account_size'] * request.session['account_risk_percent']), ",.2f")
 
-    request.session['profit_per_share'] = float(request.session['take_profit']) - float(request.session['entry'])
+    request.session['profit_per_share'] = format((request.session['take_profit']) - (request.session['entry']), ",.2f")
 
-    request.session['stop_loss_per_share'] = float(request.session['entry']) - float(request.session['stop_loss'])
+    request.session['stop_loss_per_share'] = format((request.session['entry']) - (request.session['stop_loss']), ".2f")
 
     request.session['risk_to_reward_ratio'] = float(request.session['profit_per_share']) / float(request.session['stop_loss_per_share'])
 
-    request.session['return_on_trade'] = (float(request.session['take_profit']) - float(request.session['entry'])) / float(request.session['entry'])
+    request.session['total_risk_reward'] = str(round(request.session['risk_to_reward_ratio'], 2))
+
+    request.session['return_on_trade'] = f"{(request.session['take_profit'] - request.session['entry']) / request.session['entry']: .2%}"
 
     request.session['number_of_shares'] = float(request.session['dollar_risk_on_trade']) / float(request.session['stop_loss_per_share'])
 
-    request.session['total_position_size'] = float(request.session['number_of_shares']) * float(request.session['entry'])
+    request.session['ideal_number_of_shares'] = str(round(request.session['number_of_shares'], 2))
+ 
+    request.session['total_position_size'] = format((request.session['number_of_shares']) * float(request.session['entry']), ",.2f")
    
     
     return redirect ('/trade_setup')
