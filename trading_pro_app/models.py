56fa819cb97822cf113264ae6059ed783b66f398
Robert <rfgraves86@gmail.com>
2022-04-25 16:57:52 -0600

    trade_steup class created > ORM to map out journal > autofill with trades

--- trading_pro_app/models.py
+++ trading_pro_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class trade_setup(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    # trade_type =
+    coin = models.CharField(max_length=10)
+    entry = models.DecimalField(max_digits=5, decimal_places=2)
+    stop_loss = models.DecimalField(max_digits=5, decimal_places=2)
+    take_profit = models.DecimalField(max_digits=5, decimal_places=2)
+    exit_price = models.DecimalField(max_digits=5, decimal_places=2)
+    # return_on_trade = 
+    
+
