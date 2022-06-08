cfe681ed70577ffb46f14642b7446f8e84155d3a
Robert <rfgraves86@gmail.com>
2022-06-08 16:19:54 -0600

    models update

--- trading_pro_app/models.py
+++ trading_pro_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class trade_setup(models.Model):
-    date = models.CharField(max_length=255)
+    # date = models.CharField(max_length=255)
     # trade_type =
     coin = models.CharField(max_length=10)
     entry = models.DecimalField(max_digits=5, decimal_places=2)
